c94ac715314a0ba28b51230d2a1bea629427a828
Shaul Kimel <shaullk600@gmail.com>
2025-07-09 12:44:10 +0300

    Final adjustments before moving to development branch

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -1,11 +1,13 @@
 // import express from "express";
 import greetRoutes from "./greetRoutes.js";
+import usersRoutes from "./usersRoutes.js";
 
-export default function configRoutes(app){
-    app.use('/greet' , greetRoutes );
-    app.use('/users')
-    app.use((req,res) =>{ // כל ניתוב שאינו קיים - יכנס לפה 
-        res.status(404).json({msg : 'Route not found'})
+export default function configRoutes(app) {
+    app.use('/greet', greetRoutes);
+    app.use('/users', usersRoutes);
+    
+    app.use((req, res) => { // כל ניתוב שאינו קיים - יכנס לפה 
+        res.status(404).json({ msg: 'Route not found' })
     });
     //חומר למחשבה  : אולי נרצה לשים פה איזשהו "logger" שיתןצ לי אינדיקצייה במידה ו"מנסים" את השרת הזה
 }
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,7 +4,10 @@ import {getAllUsers,addNewUser  } from "../controllers/usersControles.js";
 const router = express.Router();
 
 router.get('/' , getAllUsers);
+router.get('/:id' , );
 router.post('/' , addNewUser);
+router.put('/:id' ,);
+router.delete('/:id' ,);
 
 
 export default router;
