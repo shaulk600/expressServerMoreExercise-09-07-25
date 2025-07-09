be106f40a352b4b96e97682227078dbcdf3d85ce
Shaul Kimel <shaullk600@gmail.com>
2025-07-09 17:37:39 +0300

    Upload: complate new method of CRUD

--- controllers/usersControles.js
+++ controllers/usersControles.js
@@ -25,11 +25,10 @@ export async function setNewUser(req, res) {
     if (!username) return res.status(400).send('Name is required');
     if (!userId) return res.status(400).send('Id is required');
     const newData = new User(userId, username);
-
     try {
         const data = await fs.readFile(file, 'utf-8');
         const users = data ? JSON.parse(data) : [];
-        users.push(JSON.stringify(newData));
+        users.push(newData);
         await fs.writeFile(file, JSON.stringify(users));
         res.status(201).json({ message: 'User added', users });
     } catch (err) {
@@ -37,14 +36,16 @@ export async function setNewUser(req, res) {
         console.log('worning !! - server error: ', err);
     }
 }
+
 export async function getUserByID(req, res) {
     const idParam = parseInt(req.params.id || 0);
     try {
         const data = await fs.readFile(file, 'utf-8');
         const users = data ? JSON.parse(data) : [];
         for (let i = 0; i < users.length; i++) {
-            if (users[i].getId() == idParam) {
-                res.status(200).json(users[index]);
+            const idA = users[i].getId();
+            if (users[i].idA == idParam) {
+                res.status(200).json({ idA: (users[i].getName()) });
             }
         }
         res.status(404).json({ msg: 'id - User not found' });
@@ -55,10 +56,11 @@ export async function getUserByID(req, res) {
 
 }
 
-export async function updateUserByID(req, res) { //updateName
-    const index = parseInt(req.params.index || 0);
-    const { name } = req.body;
-    if (!name) return res.status(400).send('Name is required');
+export async function updateUserByID(req, res) { //updateName about index
+    const userId = req.body.id;
+    const username = req.body.name;
+    if (!username) return res.status(400).send('Name is required');
+    if (!userId) return res.status(400).send('Id is required');
 
     try {
         const data = await fs.readFile(file, 'utf-8');
@@ -66,26 +68,51 @@ export async function updateUserByID(req, res) { //updateName
         if (index < 0 || index >= users.length) {
             return res.status(404).send('User not found');
         }
-        users[index] = name;
-        await fs.writeFile(file, JSON.stringify(users));
-        res.status(200).json({ message: 'User updated', users });
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].getId() === idParam) {
+                users.splice(i, 1, json.stringify({ userId: username }));
+                await fs.writeFile(file, JSON.stringify(users));
+                res.status(200).json({ message: 'User updated', users });
+            }
+        }
+        res.status(400).json({ msg: 'Invalid request - id not found' });
     } catch {
         res.status(500).send('Error updating user');
         console.log('worning !! - server error: ', err);
     }
 }
-export async function deleteUserByID(req, res) {
-    const index = parseInt(req.params.index);
+// export async function updateUserByIndex(req, res) { //updateName about index
+//     const index = parseInt(req.params.index || 0);
+//     const { name } = req.body;
+//     if (!name) return res.status(400).send('Name is required');
 
+//     try {
+//         const data = await fs.readFile(file, 'utf-8');
+//         const users = JSON.parse(data);
+//         if (index < 0 || index >= users.length) {
+//             return res.status(404).send('User not found');
+//         }
+//         users[index] = name;
+//         await fs.writeFile(file, JSON.stringify(users));
+//         res.status(200).json({ message: 'User updated', users });
+//     } catch {
+//         res.status(500).send('Error updating user');
+//         console.log('worning !! - server error: ', err);
+//     }
+// }
+export async function deleteUserByID(req, res) {
+    const idParam = parseInt(req.params.id);
     try {
         const data = await fs.readFile(file, 'utf-8');
         const users = JSON.parse(data);
-        if (index < 0 || index >= users.length) {
-            return res.status(404).send('User not found');
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].getId() === idParam) {
+                users.splice(i, 1);
+                await fs.writeFile(file, JSON.stringify(users));
+                res.status(200).json({ message: 'User deleted', users });
+            }
         }
-        users.splice(index, 1);
-        await fs.writeFile(file, JSON.stringify(users));
-        res.status(200).json({ message: 'User deleted', users });
+        res.status(404).json({ msg: 'Invalid request - id not found'});
     } catch {
         res.status(500).send('Error deleting user');
         console.log('worning !! - server error: ', err);
